a7211d0a3f2aff76f6425449aac17f0207f7a481
gasparpd <you@example.com>
2020-02-20 17:06:53 +0100

    Ajustado el círculo al centro de la pantalla.

---
+++ EjemploGraficos/app/src/main/java/com/example/ejemplograficos/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ejemplograficos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new EjemploVista(this));
+    }
+
+    public class EjemploVista extends View {
+
+        public EjemploVista(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            int height = displayMetrics.heightPixels;
+            int width = displayMetrics.widthPixels;
+
+            //Dibujamos aquí
+            Paint pincel = new Paint();
+            pincel.setColor(Color.BLUE);
+            pincel.setStrokeWidth(8);
+            pincel.setStyle(Paint.Style.STROKE);
+            canvas.drawCircle(width/2, height/2, 100, pincel);
+        }
+    }
+}
