b928aa52db5a4fd253681d57c614486d7e0bb675
gasparpd <you@example.com>
2020-02-27 17:01:06 +0100

    Agregados getters y setters

--- Asteroides/app/src/main/java/com/gemma/asteroides/Grafico.java
+++ Asteroides/app/src/main/java/com/gemma/asteroides/Grafico.java
@@ -65,6 +65,118 @@ public class Grafico {
     public boolean verificaColision(Grafico g) {
         return (distancia(g) < (radioColision + g.radioColision));
     }
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public int getCenX() {
+        return cenX;
+    }
+
+    public void setCenX(int cenX) {
+        this.cenX = cenX;
+    }
+
+    public int getCenY() {
+        return cenY;
+    }
+
+    public void setCenY(int cenY) {
+        this.cenY = cenY;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public int getAlto() {
+        return alto;
+    }
+
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
+    public double getIncX() {
+        return incX;
+    }
+
+    public void setIncX(double incX) {
+        this.incX = incX;
+    }
+
+    public double getIncY() {
+        return incY;
+    }
+
+    public void setIncY(double incY) {
+        this.incY = incY;
+    }
+
+    public double getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(double angulo) {
+        this.angulo = angulo;
+    }
+
+    public double getRotacion() {
+        return rotacion;
+    }
+
+    public void setRotacion(double rotacion) {
+        this.rotacion = rotacion;
+    }
+
+    public int getRadioColision() {
+        return radioColision;
+    }
+
+    public void setRadioColision(int radioColision) {
+        this.radioColision = radioColision;
+    }
+
+    public int getxAnterior() {
+        return xAnterior;
+    }
+
+    public void setxAnterior(int xAnterior) {
+        this.xAnterior = xAnterior;
+    }
+
+    public int getyAnterior() {
+        return yAnterior;
+    }
+
+    public void setyAnterior(int yAnterior) {
+        this.yAnterior = yAnterior;
+    }
+
+    public int getRadioInval() {
+        return radioInval;
+    }
+
+    public void setRadioInval(int radioInval) {
+        this.radioInval = radioInval;
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public void setView(View view) {
+        this.view = view;
+    }
 }
 
 
