666de5335a056d86eef01ec1e5cf1c693fcbadb0
gasparpd <gasparzurea1604@gmail.com>
2020-03-14 15:40:53 +0100

    Creada la clase ObjetoCotidiano con los atributos "nombre" e "imagen"

--- AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetoCotidiano.java
+++ AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetoCotidiano.java
@@ -1,4 +1,31 @@
 package com.example.appaprendiendoidiomas;
 
 public class ObjetoCotidiano {
+
+    private String nombre;
+    private int imagen;
+
+    public ObjetoCotidiano(String nombre, int imagen) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+    }
+
+    public ObjetoCotidiano() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
 }
