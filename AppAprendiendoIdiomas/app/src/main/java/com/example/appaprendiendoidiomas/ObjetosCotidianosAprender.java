45fa89f58c2eba4aa7ad4bf59236759298d9225b
gasparpd <gasparzurea1604@gmail.com>
2020-03-14 15:45:10 +0100

    Instanciados los atributos del main de objetosCotidianos

--- AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetosCotidianosAprender.java
+++ AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetosCotidianosAprender.java
@@ -1,14 +1,21 @@
 package com.example.appaprendiendoidiomas;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ObjetosCotidianosAprender extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<ObjetoCotidiano> objetoscotidianos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_objetos_cotidianos_listview);
+
+        listView = findViewById(R.id.aprender_objetos_listview);
     }
 }
