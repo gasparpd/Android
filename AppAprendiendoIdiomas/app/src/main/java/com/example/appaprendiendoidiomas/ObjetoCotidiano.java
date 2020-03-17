bbdf252947a6cc317e7bf7e1b9add1902854b0e0
gasparpd <gasparzurea1604@gmail.com>
2020-03-17 13:47:28 +0100

    Añadido atributo 'name'

--- AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetoCotidiano.java
+++ AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetoCotidiano.java
@@ -4,10 +4,12 @@ public class ObjetoCotidiano {
 
     private String nombre;
     private int imagen;
+    private String name;
 
-    public ObjetoCotidiano(String nombre, int imagen) {
+    public ObjetoCotidiano(String nombre, int imagen, String name) {
         this.nombre = nombre;
         this.imagen = imagen;
+        this.name = name;
     }
 
     public ObjetoCotidiano() {
@@ -27,5 +29,13 @@ public class ObjetoCotidiano {
 
     public void setImagen(int imagen) {
         this.imagen = imagen;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
