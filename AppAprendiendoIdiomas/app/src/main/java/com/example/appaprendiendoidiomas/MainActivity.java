67b412932b9a197db0218d16056f88dcecc79e70
gasparpd <gasparzurea1604@gmail.com>
2020-03-17 15:14:21 +0100

    Subidas las imágenes y creado el arraylist

--- AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/MainActivity.java
+++ AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.appaprendiendoidiomas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_colores, btn_num;
@@ -27,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void numeros (View view){
         Intent i = new Intent(this, ActivityNumeros.class);
+        startActivity(i);
+    }
+
+    public void objetos(View view) {
+        Intent i = new Intent(this, ObjetosCotidianosAprender.class);
         startActivity(i);
     }
 }
--- AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetosCotidianosAprender.java
+++ AppAprendiendoIdiomas/app/src/main/java/com/example/appaprendiendoidiomas/ObjetosCotidianosAprender.java
@@ -1,15 +1,17 @@
 package com.example.appaprendiendoidiomas;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
-public class ObjetosCotidianosAprender extends AppCompatActivity {
+public class ObjetosCotidianosAprender extends MainActivity {
 
     private ListView listView;
-    private ArrayAdapter<ObjetoCotidiano> objetoscotidianos;
+    private ArrayList<ObjetoCotidiano> objetoscotidianos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +19,28 @@ public class ObjetosCotidianosAprender extends AppCompatActivity {
         setContentView(R.layout.activity_objetos_cotidianos_listview);
 
         listView = findViewById(R.id.aprender_objetos_listview);
+
+        objetoscotidianos = new ArrayList<ObjetoCotidiano>();
+        objetoscotidianos.add(new ObjetoCotidiano("BALÓN", R.drawable.balon, "BALL"));
+        objetoscotidianos.add(new ObjetoCotidiano("CEPILLO DEL PELO", R.drawable.cepillo_pelo, "HAIR BRUSH"));
+        objetoscotidianos.add(new ObjetoCotidiano("BOLÍGRAFO", R.drawable.boli, "PEN"));
+        objetoscotidianos.add(new ObjetoCotidiano("CAMA", R.drawable.cama, "BED"));
+        objetoscotidianos.add(new ObjetoCotidiano("CEPILLO DE DIENTES", R.drawable.cepillo_dientes, "TOOTHBRUSH"));
+        objetoscotidianos.add(new ObjetoCotidiano("CONO", R.drawable.cono, "WORK CONE"));
+        objetoscotidianos.add(new ObjetoCotidiano("LÁPIZ", R.drawable.lapiz, "PENCIL"));
+        objetoscotidianos.add(new ObjetoCotidiano("LLAVES", R.drawable.llaves, "KEYS"));
+        objetoscotidianos.add(new ObjetoCotidiano("RELOJ", R.drawable.reloj, "WATCH"));
+        objetoscotidianos.add(new ObjetoCotidiano("TIJERAS", R.drawable.tijeras, "SCISSORS"));
+
+        //Implemetanos nuestro adaptador personalizado
+        AdaptadorObjetoCotidiano miadaptador = new AdaptadorObjetoCotidiano(this, R.layout.objetos_layout_items, objetoscotidianos);
+        listView.setAdapter(miadaptador);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ObjetosCotidianosAprender.this, objetoscotidianos.get(position).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
